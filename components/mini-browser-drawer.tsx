35362648a431b65fb9cfefda6b8a0050275c5a82
rudra-iitm <rudransh.iitm@gmail.com>
2025-01-04 12:04:11 +0530

    Corrected some styling

--- components/mini-browser-drawer.tsx
+++ components/mini-browser-drawer.tsx
@@ -19,13 +19,13 @@ export function MiniBrowserDialog({ url }: MiniBrowserDialogProps) {
       <DialogTrigger asChild>
         <Button variant="ghost" className="h-8 w-8 text-white"><GlobeLock /></Button>
       </DialogTrigger>
-      <DialogContent className="max-h-[90vh] max-w-[80vw] p-0 md:min-h-[600px] md:min-w-[800px]">
+      <DialogContent className="max-h-[90vh] max-w-[60vw] p-0 md:min-h-[600px] md:min-w-[800px]">
         <DialogHeader className="sr-only">
           <DialogTitle>Mini Browser</DialogTitle>
         </DialogHeader>
         <Safari
           url={url}
-          className="h-[90vh] md:h-[80vh] max-w-[80vw]"
+          className="h-[90vh] md:h-[80vh] max-w-[60vw]"
         />
       </DialogContent>
     </Dialog>
--- components/ui/safari.tsx
+++ components/ui/safari.tsx
@@ -26,7 +26,8 @@ export function Safari({
         {/* Window Controls */}
           <div className="flex items-center justify-start space-x-2 px-4 py-2">
           <DialogClose asChild>
-            <div className="w-3 h-3 bg-red-500 rounded-full hover:cursor-pointer"></div>
+            <div className="w-3 h-3 bg-red-500 rounded-full hover:cursor-pointer">
+            </div>
           </DialogClose>
             <div className="w-3 h-3 bg-yellow-500 rounded-full"></div>
             <div className="w-3 h-3 bg-green-500 rounded-full"></div>
@@ -55,7 +56,7 @@ export function Safari({
         </div>
 
         {/* URL Bar */}
-        <div className="mx-12 flex flex-1 items-center rounded-md bg-[#E5E5E5] px-3 py-1.5 dark:bg-[#404040]">
+        <div className="mx-8 flex flex-1 items-center rounded-md bg-[#E5E5E5] px-3 py-1.5 dark:bg-[#404040]">
           <FaShieldAlt className="text-slate-600" />
           <span className="px-2 truncate text-sm text-muted-foreground">
            {url && url.trim().length < 20 ? url : url?.trim() || "loading..."}
@@ -63,7 +64,7 @@ export function Safari({
         </div>
 
         {/* Action Buttons */}
-        <div className="hidden items-center gap-2 sm:flex mr-5">
+        <div className="hidden items-center gap-2 sm:flex">
           <button 
             className="rounded-md p-1 text-muted-foreground hover:bg-accent"
             aria-label="Share"
@@ -86,7 +87,7 @@ export function Safari({
       </div>
 
       {/* Content Area */}
-      <div className="relative flex-1 bg-white">
+      <div className="relative flex-1 bg-[#E5E5E5]">
         <iframe
           className="absolute h-full w-full border-0"
           sandbox="allow-same-origin allow-scripts allow-popups allow-forms"
